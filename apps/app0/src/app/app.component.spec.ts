ee447822a41e06cd9c6a8300c1027970858483ed
Jefiozie <jefiozie.bosch@gmail.com>
2023-01-03 13:17:44 +0100

    fix: tests

--- apps/app0/src/app/app.component.spec.ts
+++ apps/app0/src/app/app.component.spec.ts
@@ -1,10 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { Lib0parentComponent } from '@nx-playground/app0/lib0/lib0';
+import { Lib1parentComponent } from '@nx-playground/app0/lib1/lib1';
+import { Lib2parentComponent } from '@nx-playground/app0/lib2/lib2';
+import { Lib3parentComponent } from '@nx-playground/app0/lib3/lib3';
+import { Lib4parentComponent } from '@nx-playground/app0/lib4/lib4';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({ declarations: [AppComponent, NxWelcomeComponent] }).compileComponents();
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent, NxWelcomeComponent],
+      imports: [
+        Lib0parentComponent,
+        Lib1parentComponent,
+        Lib2parentComponent,
+        Lib3parentComponent,
+        Lib4parentComponent,
+      ],
+    }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -17,12 +31,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app0');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome app0');
   });
 });
